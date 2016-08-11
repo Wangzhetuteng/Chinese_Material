d9a21dea73c66b26fd861efb531c71b9567ef06c
Wangzhetuteng <ericdicaprio@outlook.com>
2016-08-11 18:52:49 -0400

    added ErrorController and modified web.config to promote custom error pages

---
+++ COMP2007-S2016-TeamProject2/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace COMP2007_S2016_TeamProject2.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View("NotFound");
+        }
+    }
+}
